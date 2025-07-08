9b6e7ea0d7420f7498b47cb31ad323c25b344813
jheniferlima1508 <jhenifer.g.lima@aluno.senai.br>
2025-07-08 10:57:55 -0500

    subindo a atividade

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,31 +6,28 @@ CREATE TABLE Paises(
 
 Id INT,
 
-Nome Varchar (100),
+Nome Varchar (50),
 
-Continente Varchar(50),
+Continente VARCHAR (100),
 
-Populacao Varchar (100),
+populacao BIGINT ,
 
-Area_km2 Varchar (100),
+area_km2 BIGINT ,
 
-Idioma_oficial Varchar (50)
-
+idioma_oficial VARCHAR (50),
 );
 
 SELECT * FROM Paises;
-
-DROP TABLE Paises;
 
 CREATE TABLE Economia(
 
 Id INT,
 
-Pais_id Varchar (100),
+Pais_id INT,
 
-Pib_usd Varchar (100),
+Pib_usd DECIMAL (38),
 
-Pib_per_capita_usd Varchar (100),
+Pib_per_capita_usd  DECIMAL (30),
 
 Ano Varchar (100)
 
@@ -39,7 +36,7 @@ Ano Varchar (100)
 SELECT * FROM Economia;
 
 INSERT INTO Economia (Id, Pais_id, Pib_usd, pib_per_capita_usd, Ano)
-VALUES ('8','260', '1.000.000.000', '15.000' , '2025');
+VALUES ('4','260', '1.000.000.000', '15.000' , '2024');
 
 SELECT * FROM Economia 
 SELECT * FROM Paises
@@ -53,10 +50,5 @@ VALUES ('4','Egito', 'Africa', '700,' , '6.300 km', 'Arabe');
 
 INSERT INTO Paises (Id, Nome , Continente , Populacao , Area_km2 , Idioma_oficial)
 VALUES ('4','França', 'Europa', '900,' , '3.100 km', 'Frances');
-
-
-
-
-
 
 
